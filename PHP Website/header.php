25afc50505261b23d238081376e27b5677b366f4
Gina Levy <ginalevy@knights.ucf.edu>
2016-04-24 13:48:58 -0400

    Added files via upload

---
+++ PHP Website/header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+<title>My PHP Template</title>
+</head>
+<body>
+	<div id="header">
+	<div id="logo">
+		<p>LOGO</p>
+	</div>
+<div id="hsubscribe">
+	<p>Header Subscribe</p>
+</div>
+</div>
+<div id="navbar">
+	<p>navbar</p>
+</div>
